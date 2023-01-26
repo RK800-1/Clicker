85c27529e6473f72058afc248c8d43c74418e7aa
Anon <aizeknev8@gmail.com>
2023-01-26 11:47:28 +0300

    Clean-Up

---
+++ Assets/Scripts/PlayScene.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// This class is responsible for
+/// the scene management +
+/// game behaviour
+/// </summary>
+public class PlayScene : MonoBehaviour
+{
+    [SerializeField] int TotalBugsFixed;
+    [SerializeField] Text AmountOfMoney;
+
+    private void Start()
+    {
+        TotalBugsFixed = PlayerPrefs.GetInt("TotalBugsFixed");
+    }
+
+    public void FixBugBtnClick()
+    {
+        TotalBugsFixed++;
+        PlayerPrefs.SetInt("TotalBugsFixed", TotalBugsFixed);
+    }
+
+    public void To_Shop()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void To_Profile()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void To_About()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    void FixedUpdate()
+    {
+        AmountOfMoney.text = TotalBugsFixed.ToString();
+    }
+}
